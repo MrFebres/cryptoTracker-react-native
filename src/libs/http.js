d1c4d0555387e2f50f6468e35d65f0660af11a66
Freddy Febres <freddyrfa@gmail.com>
2021-01-18 20:28:23 -0300

    First commit

---
+++ src/libs/http.js
@@ -0,0 +1,30 @@
+export class Http {
+  static instance = new Http();
+
+  get = async (url) => {
+    try {
+      let req = await fetch(url);
+      let json = await req.json();
+
+      return json;
+    } catch (err) {
+      console.info('[FETCH-ERR] ', err);
+      throw new Error();
+    }
+  };
+
+  post = async (url, body) => {
+    try {
+      let req = await fetch(url, {
+        method: 'POST',
+        body,
+      });
+      let json = await req.json();
+
+      return json;
+    } catch (err) {
+      console.info('[FETCH-ERR] ', err);
+      throw new Error();
+    }
+  };
+}
